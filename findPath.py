51357ae70e2b6ce40d7115d887ac8ab9e560287f
zhuojimmy <zhuo_jimmy@yeah.net>
2016-05-11 18:32:37 +0800

    惊险刺激的三跳

--- findPath.py
+++ findPath.py
@@ -11,7 +11,7 @@ def getResult(id1,id2):
     #allPath = oneHop.getPath(id1,id2) + twoHop.getPath(id1,id2) + threeHop.getPath(id1,id2)
     #return str(allPath)
     #twoHop.getPath(id1,id2) 
-    return oneHop.one_hop_path(id1,id2)+newTwoHop.two_hop_path(id1,id2)#+threeHop.three_hop_path(id1,id2)
+    return oneHop.one_hop_path(id1,id2)+newTwoHop.two_hop_path(id1,id2)+threeHop.three_hop_path(id1,id2)
     #最终返回list
     
 # test url : http://localhost/?id1=2147152072&id2=189831743
